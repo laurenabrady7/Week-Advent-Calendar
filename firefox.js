02d7b35fa2b33f11d19bc6ee6b3bccb3cc41f224
laurenabrady7 <bradl368@newschool.edu>
2016-11-01 17:15:06 -0400

    javascript and html files

---
+++ firefox.js
@@ -0,0 +1,138 @@
+console.log("working");
+
+var block0 = document.getElementById("block0");
+var b0 = block0.children;
+console.log(b0);
+
+block0.addEventListener("click",function(){
+    
+    for(var i = 0; i < b0.length; i++){
+        if(b0[i].tagName=="P"){
+            b0[i].style.display = 'block';
+        }
+    }
+
+});
+
+
+var block1 = document.getElementById("block1");
+var b1 = block1.children;
+console.log(b1);
+
+block1.addEventListener("click",function(){
+    
+    for(var i = 0; i < b1.length; i++){
+        if(b1[i].tagName=="P"){
+            b1[i].style.display = 'block';
+        }
+    }
+
+});
+
+
+var block2 = document.getElementById("block2");
+var b2 = block2.children;
+console.log(b2);
+
+block2.addEventListener("click",function(){
+    
+    for(var i = 0; i < b2.length; i++){
+        if(b2[i].tagName=="P"){
+            b2[i].style.display = 'block';
+        }
+    }
+
+});
+
+
+var block2 = document.getElementById("block2");
+var b2 = block2.children;
+console.log(b2);
+
+block2.addEventListener("click",function(){
+    
+    for(var i = 0; i < b2.length; i++){
+        if(b2[i].tagName=="P"){
+            b2[i].style.display = 'block';
+        }
+    }
+
+});
+
+
+var block3 = document.getElementById("block3");
+var b3 = block3.children;
+console.log(b3);
+
+block3.addEventListener("click",function(){
+    
+    for(var i = 0; i < b3.length; i++){
+        if(b3[i].tagName=="P"){
+            b3[i].style.display = 'block';
+        }
+    }
+
+});
+
+
+var block4 = document.getElementById("block4");
+var b4 = block4.children;
+console.log(b4);
+
+block4.addEventListener("click",function(){
+    
+    for(var i = 0; i < b4.length; i++){
+        if(b4[i].tagName=="P"){
+            b4[i].style.display = 'block';
+        }
+    }
+
+});
+
+
+var block5 = document.getElementById("block5");
+var b5 = block5.children;
+console.log(b5);
+
+block5.addEventListener("click",function(){
+    
+    for(var i = 0; i < b5.length; i++){
+        if(b5[i].tagName=="P"){
+            b5[i].style.display = 'block';
+        }
+    }
+
+});
+
+
+var block6 = document.getElementById("block6");
+var b6 = block6.children;
+console.log(b6);
+
+block6.addEventListener("click",function(){
+    
+    for(var i = 0; i < b6.length; i++){
+        if(b6[i].tagName=="P"){
+            b6[i].style.display = 'block';
+        }
+    }
+
+});
+
+
+var block7 = document.getElementById("block7");
+var b7 = block7.children;
+console.log(b7);
+
+block7.addEventListener("click",function(){
+    
+    for(var i = 0; i < b7.length; i++){
+        if(b7[i].tagName=="P"){
+            b7[i].style.display = 'block';
+        }
+    }
+
+});
+
+
+
